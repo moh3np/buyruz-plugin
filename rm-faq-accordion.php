63ce7af5eb390b01484150b893060b21d4c8b53f
Mohsen Pazhoohiyani <moh3n@Mohsens-MacBook-Pro.local>
2025-10-24 21:54:43 +0330

    chore: bump version to 1.3.10

--- rm-faq-accordion.php
+++ rm-faq-accordion.php
@@ -3,7 +3,7 @@
  * Plugin Name: تنظیمات بایروز
  * Plugin URI: https://github.com/Codruz/buyruz-plugin.git
  * Description: تنظیمات بایروز، مرکز مدیریت امکانات اختصاصی افزونه شامل آکاردئون FAQ و سایر قابلیت‌های سفارشی. از اینجا می‌توانید رفتار افزونه‌های بایروز را تنظیم کنید.
- * Version: 1.3.9
+ * Version: 1.3.10
  * Author: کُدروز
  * Author URI: https://codruz.ir
  * License: Proprietary
